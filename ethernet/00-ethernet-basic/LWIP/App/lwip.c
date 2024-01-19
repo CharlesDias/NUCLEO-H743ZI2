681965163abad6e7df0d315806802c6ce497d5bc
Charles Dias <charlesdias.cd@outlook.com>
2024-01-19 17:06:03 -0300

    feat(00-ethernet-basic): add MX_LWIP_Process and set uo LEDs to flag when the cable connection changed"

--- ethernet/00-ethernet-basic/LWIP/App/lwip.c
+++ ethernet/00-ethernet-basic/LWIP/App/lwip.c
@@ -168,11 +168,15 @@ static void ethernet_link_status_updated(struct netif *netif)
   if (netif_is_up(netif))
   {
 /* USER CODE BEGIN 5 */
+  HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, SET);
+  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, RESET);
 /* USER CODE END 5 */
   }
   else /* netif is down */
   {
 /* USER CODE BEGIN 6 */
+  HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, RESET);
+  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, SET);
 /* USER CODE END 6 */
   }
 }
